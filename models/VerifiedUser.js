36f576dcaab08e56c21f73781b10a3a9e99ac19f
Virajing <164054155+Virajing@users.noreply.github.com>
2024-11-19 16:23:16 +0530

    Add files via upload

---
+++ models/VerifiedUser.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const verifySchema = new mongoose.Schema({
+    registration_number: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: Date,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('verify', verifySchema);
